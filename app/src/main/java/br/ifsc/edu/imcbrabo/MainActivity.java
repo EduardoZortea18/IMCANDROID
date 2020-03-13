cc6d62157e22e60e2d6e7036fd68881f19b305db
aluno <runefrioo@gmail.com>
2020-03-13 08:28:55 -0300

    Initial commit

---
+++ app/src/main/java/br/ifsc/edu/imcbrabo/MainActivity.java
@@ -0,0 +1,26 @@
+package br.ifsc.edu.imcbrabo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+
+    public void  abrirApp(View view){
+
+        if(view==findViewById(R.id.btnIMC)){
+            Intent intent = new Intent(this,CalcularIMC.class);
+            startActivity(intent);
+        }
+
+    }
+}
